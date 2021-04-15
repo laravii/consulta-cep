91e6b02e83de9b3798241c61a4ce37620b78dccd
Lara Santos <laravitoria1@hotmail.com>
2021-04-15 15:22:12 -0300

    [FEAT] create and implemete search adress form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,138 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Form as FormikForm, Field, Formik } from 'formik'
+import React, { useState } from 'react'
+import * as yup from 'yup'
+import { Button, Card, Form } from 'react-bootstrap'
+import './styles/global.css'
+import { api } from './services/api'
+import { api_keys } from './services/api_keys'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Inputs {
+  cep: string
+  adress: string
+  city: string
+  locale: string
+  burgh: string
+}
+const initialValues: Inputs = {
+  cep: '',
+  adress: '',
+  city: '',
+  locale: '',
+  burgh: '',
 }
 
-export default App;
+const validationSchema = yup.object().shape({
+  cep: yup
+    .string()
+    .test('len', 'Por favor, digite o CEP completo', (val) => val?.length === 8)
+    .required(),
+  adress: yup.string().required(),
+  city: yup.string().required(),
+  locale: yup.string().required(),
+})
+function App() {
+  const [getError, setGetError] = useState('')
+  const handleSubmit = (values: Inputs) => {
+    console.log(values)
+  }
+  const onBlur = async (ev: any, setFieldValue: any) => {
+    const target = ev.target
+    await api
+      .get(`${target.value}${api_keys}`)
+      .then((res) => {
+        const data = res.data
+        if (!data.error) {
+          setFieldValue('adress', data.endereco)
+          setFieldValue('burgh', data.bairro)
+          setFieldValue('city', data.cidade)
+          setFieldValue('locale', data.uf)
+          setGetError('')
+        } else {
+          setGetError(`${data.error}, por favor digite o CEP correto`)
+        }
+      })
+      .catch((err) => err)
+  }
+  return (
+    <Card className="container">
+      <Card.Header className="header">
+        <h4>Encontre o endereço</h4>
+      </Card.Header>
+      <Card.Body>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {(props) => (
+            <FormikForm>
+              <Form.Group controlId="cep">
+                <p>{getError}</p>
+                <Form.Label>CEP</Form.Label>
+                <Field
+                  id="cep"
+                  className="form-control"
+                  name="cep"
+                  type="text"
+                  maxLength="8"
+                  placeholder="01010100"
+                  onBlur={(ev: any) => {
+                    onBlur(ev, props.setFieldValue)
+                  }}
+                  value={props.values.cep}
+                />
+                <h6>somente números</h6>
+              </Form.Group>
+              <p>{props.errors.cep}</p>
+              <Form.Group controlId="adress">
+                <Form.Label>Logradouro</Form.Label>
+                <Field
+                  className="form-control"
+                  id="adress"
+                  name="adress"
+                  type="text"
+                />
+                <p>{props.errors.adress}</p>
+              </Form.Group>
+              <Form.Group controlId="burgh">
+                <Form.Label>Logradouro</Form.Label>
+                <Field
+                  className="form-control"
+                  id="burgh"
+                  name="burgh"
+                  type="text"
+                />
+                <p>{props.errors.burgh}</p>
+              </Form.Group>
+              <Form.Group controlId="city">
+                <Form.Label>Estado</Form.Label>
+                <Field
+                  className="form-control"
+                  name="city"
+                  id="city"
+                  type="text"
+                />
+                <p>{props.errors.city}</p>
+              </Form.Group>
+              <Form.Group controlId="locale">
+                <Form.Label>UF</Form.Label>
+                <Field
+                  className="form-control"
+                  id="locale"
+                  name="locale"
+                  type="text"
+                />
+                <p>{props.errors.locale}</p>
+              </Form.Group>
+              <Button variant="info" type="submit">
+                Enviar
+              </Button>
+            </FormikForm>
+          )}
+        </Formik>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default App
---
+++ src/services/api_keys_example.ts
@@ -0,0 +1,5 @@
+const app_key = 'api_key'
+const app_secret = 'app_secret'
+
+export const api_keys = `/?${app_key}&${app_secret}`
+//você pode conseguir esse dados com um cadastro gratuito no site: https://webmaniabr.com/docs/rest-api-cep-ibge/
